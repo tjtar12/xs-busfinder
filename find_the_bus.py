1d6400f48751557ef6b8c3b5b86570283c41c31e
Thomas Tar <thomas.j.tar@gmail.com>
2018-02-27 09:12:46 -0500

    resize

--- find_the_bus.py
+++ find_the_bus.py
@@ -79,11 +79,12 @@ while True:
 		break
 
 
-	cv2.imshow("Bus Finder - Wide Angle", frame)
+
 
 	# resize the frame, convert it to grayscale, and blur it
-	#frame = imutils.resize(frame, width=500)
+	frame = imutils.resize(frame, width=500)
 
+	cv2.imshow("Bus Finder - Wide Angle", frame)
 	if args.get("video", None) is None:
 		crop_img = frame[100:200, 250:350]
 	else:
